a37c9ca3abf3b7efea3f1b884e18eee5407edcf1
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-05-02 22:22:32 -0500

    First commit

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+const { argv } = require('yargs');
+
+const { a, b, funcion } = argv;
+
+switch (funcion) {
+    case 'sumar':
+        console.log('>>> Suma: ', a + b);
+        break;
+    case 'restar':
+        console.log('>>> Resta: ', a - b);
+        break;
+    default:
+        console.log('sin operación');
+}
+
+//Comentario de prueba
